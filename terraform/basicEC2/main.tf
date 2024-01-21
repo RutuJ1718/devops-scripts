08775767319506ffb207542a05470323e2104538
Rutu Jokhakar <rutujokhakar7@gmail.com>
2024-01-20 19:48:17 -0800

    feat: enhancing vpc and subnet

--- terraform/basicEC2/main.tf
+++ terraform/basicEC2/main.tf
@@ -1,11 +1,27 @@
 provider "aws" {
-    region = "us-east-1"  
+  region = "us-east-1"
 }
 
 resource "aws_instance" "Server" {
-    ami = var.OS
-    instance_type = var.Size
-    tags = {
-        name = var.Name
-    }
+  ami           = var.OS
+  instance_type = var.Size
+  subnet_id     = aws_subnet.my_subnet.id
+  tags = {
+    name = var.Name
+  }
+}
+
+#AWS VPC
+resource "aws_vpc" "my_vpc" {
+  cidr_block = var.vpc_cidr
+  tags       = var.vpc_tag
+}
+
+#AWS Subnet attached to AWS VPC
+resource "aws_subnet" "my_subnet" {
+  vpc_id            = aws_vpc.my_vpc.id
+  cidr_block        = var.subnet_cidr
+  availability_zone = var.AZ
+
+  tags = var.subnet_name
 }
--- terraform/basicEC2/variables.tf
+++ terraform/basicEC2/variables.tf
@@ -1,17 +1,46 @@
 variable "OS" {
-    type = string
-    default = "ami-0c7217cdde317cfec"
-    description = "This is my ami ID"
+  type        = string
+  default     = "ami-0c7217cdde317cfec"
+  description = "This is my ami ID"
 }
 
 variable "Size" {
-    type = string
-    default = "t2.micro"
-    description = "This is my EC2 Instance Size"
+  type        = string
+  default     = "t2.micro"
+  description = "This is my EC2 Instance Size"
 }
 
 variable "Name" {
-    type = string
-    default = "WebServer"
-    description = "This is my EC2 Instance name"
+  type        = string
+  default     = "WebServer"
+  description = "This is my EC2 Instance name"
+}
+
+variable "vpc_cidr" {
+  type    = string
+  default = "172.17.0.0/16"
+}
+
+variable "vpc_tag" {
+  type = map(any)
+  default = {
+    "Name" = "test-vpc"
+  }
+}
+
+variable "subnet_cidr" {
+  type    = string
+  default = "172.17.10.0/24"
+}
+
+variable "subnet_name" {
+  type = map(any)
+  default = {
+    "Name" = "test-subnet"
+  }
+}
+
+variable "AZ" {
+  type    = string
+  default = "us-east-1a"
 }
